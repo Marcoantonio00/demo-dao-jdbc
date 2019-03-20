1fc7d0cf305060f642bdb38909664e2ff8f0358d
Marco Ferreira <marcoantonio00@outlook.com>
2019-03-20 14:07:21 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,6 +24,12 @@ public class Program {
 			System.out.println(obj);
 		}
 		
+		System.out.println("\n===Teste 03: Seller findAll===");
+		list =  sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+
 	}
 
 }
